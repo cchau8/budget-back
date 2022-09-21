6e1cba64016ec6a7458ad6a156a88c85927233c3
Cedric Chau <cedricchau87@gmail.com>
2022-09-21 22:46:50 +0200

    feat: mongoose connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
+mongoose.connect("mongodb://localhost:27017/budget", {}, () =>
+    console.log("✔ [server]: connected to db")
+);
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });

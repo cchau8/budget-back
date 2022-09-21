0d5f4bc7071a6dccf70a229f70928099c81c58ff
Cedric Chau <cedricchau87@gmail.com>
2022-09-21 23:30:53 +0200

    feat: register route basic

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,33 @@
 import express, { Request, Response, Router } from "express";
 import bcrypt from "bcrypt";
 // models
+import User from "~/models/User";
 
 const router: Router = express.Router();
 
 router.post("/register", async (req: Request, res: Response) => {
-    // const hash = bcrypt.hashSync(req.body.password, 10);
-    // console.log(hash);
+    try {
+        const { username, email, password } = req.body;
+        const duplicateUser = await User.findOne({
+            $or: [{ username: username }, { email: email }],
+        });
+
+        if (duplicateUser) {
+            return res.status(400).json({
+                message: "The username or the email you're trying to use is already used.",
+            });
+        }
+
+        const hash = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+        });
+        user.save();
+        return res.status(200).json({ message: "New user successfully created ! 🔥" });
+    } catch (error) {}
 });
 
 export default router;

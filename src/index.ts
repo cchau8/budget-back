b61c6c691b693613958a4ba620ef157c32fa7817
Cedric Chau <cedricchau87@gmail.com>
2022-09-22 17:28:35 +0200

    add: type user in req

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ import userRoutes from "./routes/user.routes";
 
 dotenv.config();
 
+declare global {
+    namespace Express {
+        export interface Request {
+            user?: any;
+        }
+    }
+}
+
 const app: Express = express();
 const port = process.env.PORT;
 mongoose.connect("mongodb://localhost:27017/budget", {}, () =>

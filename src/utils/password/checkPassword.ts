e6474e5569ccf6f74469e3f394f15e36f5b22418
Cedric Chau <cedricchau87@gmail.com>
2022-09-22 17:35:02 +0200

    util: checkPassword

---
+++ src/utils/password/checkPassword.ts
@@ -0,0 +1,7 @@
+const checkPassword = (password: string) => {
+    // the checker checks for one uppercase, one lowercase, one digit and between 8 and 30 characters
+    const checker = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,30}$/);
+    return checker.test(password);
+};
+
+export { checkPassword };

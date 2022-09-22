f168ecc064353dfc8bd1ae6c5f5b3a0863c2edb1
Cedric Chau <cedricchau87@gmail.com>
2022-09-22 17:34:48 +0200

    feat: auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "~/models/User";
+
+export interface IToken {
+    user: {
+        email: string;
+        username: string;
+        userId: string;
+    };
+}
+
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (token) {
+            const decodedToken = jwt.verify(token, "RANDOM-TOKEN");
+            req.user = (decodedToken as IToken).user;
+            next();
+        }
+    } catch (error) {
+        res.status(401).json({
+            error: new Error("Invalid request!"),
+        });
+    }
+};
+
+module.exports = auth;
